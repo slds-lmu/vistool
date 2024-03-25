f6ed49a42b50e168158e53d85554bed187a19674
Lukas Burk <jemus42@users.noreply.github.com>
2024-03-25 15:32:07 +0100

    Maintenance work (#13)

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# vistool 0.1.0
+
+* Remove `lim2002` optimizer as it's no longer exported by [`TestFunctions`](https://cran.r-project.org/package=TestFunctions).
+* Add GPL3 license in line with e.g. `mlr3`.
+* Start versioning.
--- R/Objective.R
+++ R/Objective.R
@@ -237,7 +237,8 @@ tfuns = c(list(list(minimize = TRUE, name = "branin", desc = "A function. 2 dime
   list(list(minimize = FALSE, name = "franke", desc = "A function. 2 dimensional function.", xdim = 2, limits_lower = c(-0.5, -0.5), limits_upper = c(1, 1))),
   list(list(minimize = FALSE, name = "zhou1998", desc = "A function. 2 dimensional function.", xdim = 2, limits_lower = c(0, 0), limits_upper = c(1, 1))),
   list(list(minimize = TRUE, name = "currin1991", desc = "A function. 2 dimensional function.", xdim = 2, limits_lower = c(0, 0), limits_upper = c(1, 1))),
-  list(list(minimize = FALSE, name = "lim2002", desc = "Some function? 2 dimensional function.", xdim = 2, limits_lower = c(0, 0), limits_upper = c(1, 1))),
+  # lim2002 no longer available as of 2024-03-25
+  # list(list(minimize = FALSE, name = "lim2002", desc = "Some function? 2 dimensional function.", xdim = 2, limits_lower = c(0, 0), limits_upper = c(1, 1))),
   list(list(minimize = FALSE, name = "banana", desc = "A banana shaped function. 2 dimensional function.", xdim = 2, limits_lower = c(0, 0), limits_upper = c(1, 1))),
   list(list(minimize = FALSE, name = "sinumoid", desc = "A sinusoid added to a sigmoid function. 2 dimensional function.", xdim = 2, limits_lower = c(0, 0), limits_upper = c(1, 1))),
   list(list(minimize = FALSE, name = "waterfall", desc = "A sinusoid added to a sigmoid function. 2 dimensional function.", xdim = 2, limits_lower = c(0, 0), limits_upper = c(1, 1))),
@@ -272,10 +273,26 @@ tfuns = c(list(list(minimize = TRUE, name = "branin", desc = "A function. 2 dime
 for (i in seq_along(tfuns)) {
   tf = tfuns[[i]]
   id = sprintf("TF_%s", tf$name)
-  cl = sprintf("TestFunctions::%s", tf$name)
-  suppressWarnings(dict_objective$add(id, Objective$new(fun = eval(parse(text = cl)),
-    id = id, label = tf$name, xdim = tf$xdim, limits_lower = tf$limits_lower,
-    limits_upper = tf$limits_upper)))
+  # cl = sprintf("TestFunctions::%s", tf$name)
+
+  cl_fun = tryCatch({
+    utils::getFromNamespace(tf$name, "TestFunctions")
+  }, error = function(e) {
+    "skip"
+  })
+
+  if (identical(cl_fun, "skip")) {
+    message("Error retrieving '", tf$name, "' from namespace TestFunctions, skipping.")
+    next
+  }
+
+  suppressWarnings(dict_objective$add(
+      id, Objective$new(
+        fun = cl_fun,
+        id = id, label = tf$name, xdim = tf$xdim, limits_lower = tf$limits_lower,
+        limits_upper = tf$limits_upper
+      )
+  ))
 }
 
 #' @title Retrieve Objective Functions
